819145598e6c055f464a68c531b3ee3a99c2571f
Jeremy Ong <jeremy.ongws@gmail.com>
2016-03-30 18:09:38 +0800

    Initial Commit

---
+++ GuessingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuessingGame
+//
+//  Created by Jeremy Ong on 30/03/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
